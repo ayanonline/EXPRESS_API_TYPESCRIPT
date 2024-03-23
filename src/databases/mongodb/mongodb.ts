ec4130e83d95bd4155c81a2bd68a57ac1151fb7f
ayanonline <ayanghosh1389@gmail.com>
2024-03-23 15:10:57 +0530

    first commit

---
+++ src/databases/mongodb/mongodb.ts
@@ -0,0 +1,7 @@
+import { connect } from "mongoose";
+
+export default async function mongooseConnect(): Promise<void> {
+  const mongoDBURI = process.env.MONGODB_URI ?? "mongodb://127.0.0.1:27017";
+
+  await connect(mongoDBURI);
+}
